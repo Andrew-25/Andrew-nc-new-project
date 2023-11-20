6a16646de33dbb76f2541da094ae4c16440c4041
Andrew-25 <andrewoozs@hotmail.com>
2023-11-20 12:24:24 +0000

    task 1 complete

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const {
     getApi,
-    getTopics
+    // getTopics
 } = require('./controllers/app.controller');
 
 app.use(express.json());
 
 app.get('/api/healthcheck', getApi);
-app.get('/api/topics', getTopics)
+// app.get('/api/topics', getTopics)
 
 
 module.exports = app;
