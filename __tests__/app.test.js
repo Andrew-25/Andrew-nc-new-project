cb7770d64be19bb52da5eb43ee7712c931f40602
Andrew-25 <andrewoozs@hotmail.com>
2023-11-20 15:22:48 +0000

    task 3 complete

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -15,7 +15,7 @@ afterAll(() => {
 
 describe('GET Requests', () => {
     describe('GET API', () => {
-        test('should return a 200 and a message saying working', () => {
+        test('/api/healthcheck should return a 200 and a message saying working', () => {
             return request(app)
                 .get('/api/healthcheck')
                 .expect(200)
@@ -24,7 +24,7 @@ describe('GET Requests', () => {
                     expect(body.msg).toBe('working');
                 });
         });
-        test('should return a 404 if supplies an invalid endpoint', () => {
+        test('/api/notanendpoint should return a 404 if supplies an invalid endpoint', () => {
             return request(app)
                 .get('/api/notanendpoint')
                 .expect(404)
@@ -32,6 +32,37 @@ describe('GET Requests', () => {
                     expect(error).not.toBe(false);
                     expect(body).toEqual({});
                 });
+        });
+        test('/api should provide a description of other endpoints that are available', () => {
+            return request(app)
+                .get('/api')
+                .expect(200)
+                .then(({ body }) => {
+                    const endpointKeys = Object.keys(body.endpoints);
+                    expect(endpointKeys.includes('GET /api')).toBe(true);
+                    expect(endpointKeys.includes('GET /api/topics')).toBe(true);
+                })
+        });
+        test('/api object objects should have keys of description, queries (an array of valid queries) and an example response.', () => {
+            return request(app)
+                .get('/api')
+                .expect(200)
+                .then(({ body }) => {
+                    const { endpoints } = body;
+                    for (const key in endpoints) {
+                        if (key === 'GET /api') {
+                            expect(endpoints[key]).toMatchObject({
+                                description: expect.any(String)
+                            });
+                        } else {
+                            expect(endpoints[key]).toMatchObject({
+                                description: expect.any(String),
+                                queries: expect.any(Array),
+                                exampleResponse: expect.any(Object)
+                            });
+                        }
+                    }
+                })
         });
     });
     describe('GET /api/topics', () => {
--- models/app.model.js
+++ models/app.model.js
@@ -1,4 +1,7 @@
 const db = require('../db/connection');
+const endpoints = require('../endpoints.json')
 const format = require("pg-format");
+
+exports.findEndpoints = () => { return Promise.resolve(endpoints)}
 
 exports.findTopics = () => { return db.query(`SELECT * FROM topics;`) };
