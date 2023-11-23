531521558000cf53618a0aa0152e45cf9c574247
Andrew-25 <andrewoozs@hotmail.com>
2023-11-23 15:41:12 +0000

    making chanes to files for hosting (task 13)

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,11 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
+const config = {}
+
+if (ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
 
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
@@ -7,6 +13,10 @@ require('dotenv').config({
 
 if (!process.env.PGDATABASE) {
   throw new Error('PGDATABASE not set');
+}
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
 module.exports = new Pool();
