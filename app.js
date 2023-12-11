0d9bf059450cdf83c364bbd709b4c612a3ed37eb
Andrew-25 <andrewoozs@hotmail.com>
2023-12-11 11:08:32 +0000

    psql issue sorted

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const {
     getApi,
@@ -13,6 +14,7 @@ const {
     getUsers,
 } = require('./controllers/app.controller');
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/api/healthcheck', getApi);
